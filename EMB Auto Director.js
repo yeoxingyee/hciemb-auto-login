74ba5916ddab839cd289e6b2366608ed407effdf
yeoxingyee <yeoxingyee30@gmail.com>
2015-04-18 21:26:19 +0800

    Create EMB Auto Director.js

---
+++ EMB Auto Director.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         EMB Auto Director
+// @namespace    http://www2.hci.edu.sg/t0111066c
+// @version      0.1
+// @description  auto login script for EMB HCI
+// @author       Yeo Xing Yee
+// @match        http://messages.hci.edu.sg/smb/index.html
+// @grant        none
+// ==/UserScript==
+window.onload=
+    window.location="http://messages.hci.edu.sg/smb/hs_student/";
